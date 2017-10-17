3c09f3c36c74692672408f639c0cf4aa1076e87f
Samuel Würgler <samuel.wuergler@students.fhnw.ch>
2017-10-17 15:11:21 +0200

    Worked on calculator during lesson

--- src/integration-test/groovy/mvc/CalculatorSpec.groovy
+++ src/integration-test/groovy/mvc/CalculatorSpec.groovy
@@ -2,6 +2,9 @@ package mvc
 
 import geb.spock.GebSpec
 import grails.test.mixin.integration.Integration
+import spock.lang.Unroll
+
+import javax.xml.transform.Result
 
 /**
  * See http://www.gebish.org/manual/current/ for more instructions
@@ -9,26 +12,30 @@ import grails.test.mixin.integration.Integration
 @Integration
 class CalculatorSpec extends GebSpec {
 
-
-    void "Basic calculation"() {
+    @Unroll
+    void "Basic calculation: with en #enInput und exam #examInput should give result #result"() {
         when:
             go '/static/GradeCalculator.html'
         then:
         	title == "Grade Calculator"
 
         when: "set valid input"
-            $("form").en   = 5.0
-            $("form").exam = 6.0
+            $("form").en   = enInput
+            $("form").exam = examInput
             $("input", type: "submit").click()
 
         then: "Result Page is displayed"
             title == "Average"
-//            $("output").text() == "5.5"
+            $("output").text() == result
+
+        where:
+            enInput | examInput | result
+            4.0     | 5.0       | "4.5"
+            5.0     | 4.0       | "4.5"
+            1.0     | 6.0       | "3.5"
+            6.0     | 6.0       | "6.0"
 
 
-        when: "click on back link"
-            $("a", text: "calculator").click()
-        then:
-            title == "Grade Calculator"
+
     }
 }
